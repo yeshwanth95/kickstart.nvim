2921f29e845e9a8f7f56f0f090da7924a2046ea2
yeshwanth95 <staryesh@gmail.com>
2024-08-10 03:16:49 +0300

    undo test commit

--- init.lua
+++ init.lua
@@ -915,7 +915,5 @@ require('lazy').setup({
   },
 })
 
--- Dummy comment for testing git commit with lazygit
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
