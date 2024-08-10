2eb53c5905594222b3392083268505b07a752907
yeshwanth95 <staryesh@gmail.com>
2024-08-10 03:16:08 +0300

    test commit

--- init.lua
+++ init.lua
@@ -915,5 +915,7 @@ require('lazy').setup({
   },
 })
 
+-- Dummy comment for testing git commit with lazygit
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
